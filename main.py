959ba313ac0ea8f408d399ca4b9c92f917ca3fce
syamshukla <shuklasyam13@gmail.com>
2022-01-22 03:20:09 -0500

    Basic Template

--- main.py
+++
@@ -1,2 +0,0 @@
-#Python file
-#Django file
---
+++ mysite/myapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+from .models import Book
+
+
+def index(request):
+    return HttpResponse("Hello World")
+
+
+def book_by_id(request, book_id):
+    book = Book.objects.get(pk=book_id)
+    return render(request, 'book_details.html', {'book': book})
